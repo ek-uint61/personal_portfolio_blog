c8dcb46badffb2684994802bc38d2585adffab01
emreklyc <messi61tr@gmail.com>
2024-06-05 12:32:31 +0300

    ESLint update

--- next.config.js
+++ next.config.js
@@ -5,23 +5,22 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: prod ? false : true,
+  disable: !prod,
+  maximumFileSizeToCacheInBytes: 10485760, // 10 MB
 });
 
 const nextConfig = {
   experimental: {
-    //largePageDataBytes: 128 * 1000, // 128KB by default
-      largePageDataBytes: 128 * 100000,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-      ],
-    },
+    largePageDataBytes: 128 * 100000,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = withPWA(nextConfig);
-
